b4f7e5228c7c1fa98c72b957d3002c7f65e8c5f5
qinhengyi-mac <qinhengyi@kuaishou.com>
2026-03-13 23:09:09 +0800

    change infer

--- train_finqa.sh
+++ train_finqa.sh
@@ -27,7 +27,7 @@ for EPOCH in $(seq 1 "$TOTAL_EPOCHS"); do
 
     python3 -m projects.finqa.train_finqa \
         algorithm.adv_estimator=grpo \
-        data.train_batch_size=384 \
+        data.train_batch_size=512 \
         data.val_batch_size=648 \
         data.max_prompt_length=3072 \
         data.max_response_length=16384 \
@@ -38,7 +38,7 @@ for EPOCH in $(seq 1 "$TOTAL_EPOCHS"); do
         actor_rollout_ref.actor.loss_agg_mode=seq-mean-token-sum \
         actor_rollout_ref.actor.ppo_mini_batch_size=64 \
         actor_rollout_ref.actor.use_dynamic_bsz=True \
-        actor_rollout_ref.actor.ppo_max_token_len_per_gpu=60000 \
+        actor_rollout_ref.actor.ppo_max_token_len_per_gpu=80000 \
         actor_rollout_ref.actor.use_kl_loss=False \
         actor_rollout_ref.actor.clip_ratio_high=0.28 \
         actor_rollout_ref.actor.kl_loss_coef=0.001 \
@@ -50,18 +50,18 @@ for EPOCH in $(seq 1 "$TOTAL_EPOCHS"); do
         actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
         actor_rollout_ref.rollout.name=vllm \
         actor_rollout_ref.rollout.mode="async" \
-        actor_rollout_ref.rollout.enforce_eager=True \
+        actor_rollout_ref.rollout.enforce_eager=False \
         actor_rollout_ref.rollout.temperature=0.7 \
-        actor_rollout_ref.rollout.gpu_memory_utilization=0.9 \
+        actor_rollout_ref.rollout.gpu_memory_utilization=0.95 \
         actor_rollout_ref.rollout.n=8 \
         actor_rollout_ref.rollout.enable_prefix_caching=True \
         actor_rollout_ref.rollout.val_kwargs.n=1 \
         actor_rollout_ref.rollout.val_kwargs.temperature=0.6 \
         actor_rollout_ref.rollout.val_kwargs.top_p=0.95 \
-        actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=81920 \
+        actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=100000 \
         actor_rollout_ref.rollout.log_prob_use_dynamic_bsz=True \
         actor_rollout_ref.ref.fsdp_config.param_offload=False \
-        actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=81920 \
+        actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=100000 \
         actor_rollout_ref.ref.log_prob_use_dynamic_bsz=True \
         actor_rollout_ref.ref.entropy_from_logits_with_chunking=True \
         actor_rollout_ref.actor.entropy_coeff=0.002 \
@@ -70,7 +70,7 @@ for EPOCH in $(seq 1 "$TOTAL_EPOCHS"); do
         trainer.critic_warmup=0 \
         trainer.logger=['console','swanlab'] \
         trainer.project_name='finqa-grpo-curriculum' \
-        trainer.experiment_name='finqa-grpo-test2' \
+        trainer.experiment_name='finqa-grpo-exp1' \
         trainer.val_before_train=False \
         trainer.n_gpus_per_node=8 \
         trainer.nnodes=1 \
@@ -80,6 +80,6 @@ for EPOCH in $(seq 1 "$TOTAL_EPOCHS"); do
         trainer.resume_mode=$RESUME_MODE \
         rllm.agent.max_steps=20 \
         rllm.stepwise_advantage.enable=False \
-        rllm.workflow.n_parallel_tasks=1536 \
+        rllm.workflow.n_parallel_tasks=2560 \
         trainer.total_epochs=1
 done
